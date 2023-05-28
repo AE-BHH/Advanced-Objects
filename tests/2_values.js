47fa8b996cbbe224e3130b43023f91bb9adcc291
Sol Laudon <sollaudon@Sols-MBP.attlocal.net>
2023-05-28 14:59:32 -0500

    add unit tests

---
+++ tests/2_values.js
@@ -0,0 +1,6 @@
+// Return an array of the object parameter's values
+
+const objValues = (obj) => {
+}
+
+module.exports = objValues;
---
+++ tests/5_mostThis.js
@@ -0,0 +1,7 @@
+// The object has a key of everyone, which is an empty function.
+// Set the value of everyone to a function returning the values of students and teachers
+
+const objMostThis = (obj, getThis) => {
+}
+
+module.exports = objMostThis;
